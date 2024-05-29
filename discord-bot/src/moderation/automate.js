6b138a32c72b0e20423f9bc963da740984607f4c
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 12:17:32 +0530

    Added: `discord-bot/src/moderation/automate.js`

---
+++ discord-bot/src/moderation/automate.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+  if (message.content === '!ping') {
+    message.channel.send('Pong!');
+  }
+});
+
+client.login('your-token-goes-here');
+
+module.exports = client;
